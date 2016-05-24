b9a06abf323b217885fbfbc2bb2c12cebbdcc699
Denys <etyned@gmail.com>
2016-05-24 16:40:48 +0300

    Fix trello auth at background

--- chrome/extension/background/background.js
+++ chrome/extension/background/background.js
@@ -2,8 +2,6 @@ import store from '../../app/store/configureStore';
 import { wrapStore } from 'react-chrome-redux';
 import { trelloInit } from '../../app/util/trello';
 
-trelloInit();
-
 wrapStore(store, {
   portName: 'timlio'
 });
@@ -12,6 +10,7 @@ chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
   // Now we have a token saved locally, as fetched from the settings page after authorization.
   if (request.command == 'saveToken') {
     localStorage.setItem('trello_token', request.token);
+    trelloInit();
     sendResponse();
     return true;
   }
