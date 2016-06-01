cb7e5f01c63e84f4a984894ad87ea14f2f1f3afc
Denys <etyned@gmail.com>
2016-06-01 16:42:50 +0300

    Refactor register-api imports

--- meteor-app/imports/startup/server/register-api.js
+++ meteor-app/imports/startup/server/register-api.js
@@ -1,13 +1,7 @@
 import '../../api/boards/server/publications.js';
-import '../../api/boards/boards-schema.js';
-import '../../api/boards/boards.js';
 
 import '../../api/lists/server/publications.js';
-import '../../api/lists/lists-schema.js';
-import '../../api/lists/lists.js';
 
 import '../../api/cards/server/publications.js';
-import '../../api/cards/cards-schema.js';
-import '../../api/cards/cards.js';
 
 import '../../api/users/server/publications.js';
