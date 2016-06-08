9c2693320e98aca0288fa4afd4c4d5ed2694a769
Denys <etyned@gmail.com>
2016-06-08 18:08:05 +0300

    Setup calendar with BarChart component

--- meteor-app/imports/ui/layouts/MainLayout.js
+++ meteor-app/imports/ui/layouts/MainLayout.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Meteor } from 'meteor/meteor';
 
-export default MainLayout = (props) => {
-  return (
-    <div>
-      {props.children}
-    </div>
-  )
-};
+export default class MainLayout extends Component {
+  loginOnClick() {
+    Meteor.loginWithTrello({}, (err) => { 
+      if (!err)
+        console.log('Success') 
+    });
+  }
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-default navbar-static-top">
+          <div className="container">
+            <div className="navbar-header">
+              <div className="navbar-brand">Timlio</div>
+            </div>
+            <div className="collapse navbar-collapse">
+              <ul className="nav navbar-nav navbar-right">
+                <li><a onClick={this.loginOnClick} href="#">Login</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="container">
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+}
--- meteor-app/imports/ui/pages/Home.js
+++ meteor-app/imports/ui/pages/Home.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
+import BarChart from '../components/BarChart';
+import Calendar from '../components/Calendar';
+import GregorianCalendar from 'gregorian-calendar';
+import enUS from 'gregorian-calendar/lib/locale/en_US';
+import moment from 'moment';
 
-export default Home = (props) => {
-  return (
-    <div>
-      Home Route
-    </div>
-  )
-};
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    const endValue = new GregorianCalendar(enUS);
+    endValue.setTime(new Date());
+    const startValue = new GregorianCalendar(enUS);
+    startValue.setTime(moment().subtract(6, 'days'));
+
+    this.state = {
+      startValue,
+      endValue
+    };
+  }
+  onChange(field, value) {
+    this.setState({
+      [field]: value
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Calendar onChange={this.onChange.bind(this)} {...this.state}/>
+        <BarChart {...this.state}/>
+      </div>
+    )
+  }
+}
+
+export default Home;
